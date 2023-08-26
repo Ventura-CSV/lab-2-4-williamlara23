c37553b4747e498f0acf5f589ce789469a8d345d
williamlara23 <wilberth_lara1@my.vcccd.edu>
2023-08-26 16:52:04 -0700

    seperating the merged str was interesting

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
+    original_str = "Python Programming"
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:18]
+    merged_str = sub2+ " " +sub1
+    print(sub2)
+    print(sub1)
+    print(merged_str)
     pass
 
 
